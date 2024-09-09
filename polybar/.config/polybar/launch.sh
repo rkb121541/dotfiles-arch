237d7e9da79f3aa610a510eeabc90f64c2b37c63
rkb121541 <rkbidarkota@gmail.com>
2024-09-09 18:29:55 -0400

    created powermenu

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,6 @@ if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload bar &
   done
-# else
-#   polybar --reload bar &
+else
+  polybar --reload bar &
 fi
